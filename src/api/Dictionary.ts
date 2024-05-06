fb5efe8827674386a779935da2c5c0e35587b053
LoukaMB <22691132+mblouka@users.noreply.github.com>
2024-05-06 12:11:50 -0400

    More work on /dictionary

--- src/api/Dictionary.ts
+++ src/api/Dictionary.ts
@@ -1,5 +1,5 @@
 import { parse } from "node-html-parser"
-import JSON5 from "json5"
+import { createContext, runInContext } from "vm"
 
 interface Example {
   phrase: string
@@ -56,6 +56,10 @@ interface Definitions {
 }
 
 export default async function dictionary(word: string) {
+  if (process.env.UNSAFE !== "true") {
+    throw new Error("The bot administrator has not enabled this command.")
+  }
+
   const text = await (
     await fetch(`https://www.arabicreference.com/q/${word}`)
   ).text()
@@ -69,6 +73,27 @@ export default async function dictionary(word: string) {
     throw new Error("Couldn't look up word.")
   }
 
+  // Create context.
+  let retrievedDef: any = undefined
+  const virtualenv = createContext({
+    PubMain: {
+      init(_: string, arr: any[][]) {
+        retrievedDef = arr[0][1]
+      },
+    },
+  })
+
+  // Retrieve definition data.
+  // Unsafe? Absolutely, which is why /dictionary is only enabled
+  // when the UNSAFE environment variable is enabled and why the command
+  // is currently considered experimental (pending the implementation of
+  // another dictionary or at least a safer object literal parser. It's not
+  // even going in the changelog (for now).
+  runInContext(script, virtualenv)
+  return retrievedDef as Definitions
+
+  /*
   const [, definitionRaw] = script.match(/\[\[\d+,\s*{(.+)}\]\]/s) ?? []
   return JSON5.parse(`{${definitionRaw}}`) as Definitions // :troll:
+  */
 }
--- src/commands/dictionary.ts
+++ src/commands/dictionary.ts
@@ -1,6 +1,7 @@
 import { BotCommand } from "."
 
 import dictionary from "../api/Dictionary"
+import embed from "../embed"
 
 export default {
   name: "dictionary",
@@ -16,8 +17,33 @@ export default {
   async command(interaction) {
     const arabicWord = interaction.options.getString("query", true).trim()
 
-    const definitions = await dictionary(arabicWord)
+    // We only need the first definition.
+    const definition = (await dictionary(arabicWord)).entries[0]
+    if (!definition) {
+      throw new Error(`Couldn't find a definition for word "${arabicWord}".`)
+    }
 
-    await interaction.editReply("guh")
+    await interaction.editReply(
+      embed({
+        title: `\u200E${arabicWord} (${definition.noun ? "noun" : "verb"})`,
+        href: `https://www.arabicreference.com/q/${arabicWord}`,
+        contents: `**NOTE**: The dictionary command is currently experimental. Accuracy of definitions may vary. This will improve over time.\n\n${
+          definition.noun?.definitions.map((def) => `- ${def}`).join("\n") ??
+          definition
+            .verb!.form_one.definitions.map((def) => `- ${def}`)
+            .join("\n")
+        }`,
+        fields: definition.verb
+          ? [
+              {
+                name: "Masadir",
+                value: definition.verb.form_one.form_one_info[0].masadir
+                  .map((md) => `${md.arabic ?? "n/a"} - ${md.pronunciation}`)
+                  .join(", "),
+              },
+            ]
+          : [],
+      })
+    )
   },
 } as BotCommand
